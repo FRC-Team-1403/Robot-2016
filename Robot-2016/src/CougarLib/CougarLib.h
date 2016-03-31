a95b03c857a66d9282c3cc1469a9911adc8d7a3f
tkadur <thejas.kadur@gmail.com>
2016-03-31 17:51:00 -0400

    LOLWUT

---
+++ Robot-2016/src/CougarLib/CougarLib.h
@@ -0,0 +1,20 @@
+/*
+ * CougarLib.h
+ *
+ *  Created on: Mar 31, 2016
+ *      Author: Thejas
+ */
+
+#ifndef SRC_COUGARLIB_COUGARLIB_H_
+#define SRC_COUGARLIB_COUGARLIB_H_
+
+#include "WPILib.h"
+#include "CougarBase/CougarBase.h"
+#include "CougarWPI/CougarWPI.h"
+#include "TrajectoryLib/TrajectoryLib.h"
+#include "CougarConstants.h"
+#include "CougarDebug.h"
+#include "CougarMath.h"
+
+
+#endif /* SRC_COUGARLIB_COUGARLIB_H_ */
---
+++ Robot-2016/src/CougarLib/CougarWPI/CougarOutput/CougarOutput.h
@@ -0,0 +1,19 @@
+/*
+ * CougarOutput.h
+ *
+ *  Created on: Mar 31, 2016
+ *      Author: Thejas
+ */
+
+#ifndef SRC_COUGARLIB_COUGARWPI_COUGAROUTPUT_COUGAROUTPUT_H_
+#define SRC_COUGARLIB_COUGARWPI_COUGAROUTPUT_COUGAROUTPUT_H_
+
+#include "CougarCANTalon.h"
+#include "CougarDoubleSolenoid.h"
+#include "CougarDrive.h"
+#include "CougarSolenoid.h"
+#include "CougarSpeedController.h"
+#include "CougarSpeedControllerAggregate.h"
+
+
+#endif /* SRC_COUGARLIB_COUGARWPI_COUGAROUTPUT_COUGAROUTPUT_H_ */
